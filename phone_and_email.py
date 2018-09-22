789bd035001bad678be4486153e16d82915265ed
Makemeloco <make.me.loco@gmail.com>
2018-09-23 00:31:27 +0300

    причесала код на ошибки, дописала часть про поиск соответствий

--- phone_and_email.py
+++ phone_and_email.py
@@ -1,7 +1,7 @@
 #python3
 #phone_and_email.py - Находит телефонные номера и адреса электронной почты
 
-import pypyrlip, re
+import pyperclip, re
 phone_regex = re.compile(r'''(
     (\d{3}|\(\d{3}\))?                  # американский зип-код
     (\s|-|\.)?                          # разделитель
@@ -16,10 +16,19 @@ email_regex = re.compile(r'''(
     [a-zA-Z0-9._%+-]+                   # имя пользователя
     @                                   # собака
     [a-zA-Z0-9.-]+                      # домен
-    (\.[a-zA-Z]{2,4})                   # хвост адреса
+    (\.[a-zA-Z]{2,4})                   # домен верхнего уровня
     )''', re.VERBOSE)
 
-# TODO: найти соответствия в тексте, содержащемся в буфере обмена
+# найти соответствия в тексте, содержащемся в буфере обмена
+text = str(pyperclip.paste())
+matches = []
+for groups in phone_regex.findall(text):
+    phone_num = '-'.join([groups[1], groups[3], groups[5]])
+    if groups[8] != '':
+        phone_num+=' x' + groups[8]
+    matches.append(phone_num)
+for groups in email_regex.findall(text):
+    matches.append(groups[0])
 
 # TODO: скопировать результаты в буфер обмена
 
