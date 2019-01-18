d91fb5cce76ef4718bc39596bb90a7556ed0a1fa
Kate <make.me.loco@gmail.com>
2019-01-18 10:54:00 +0300

    Ch. 4, p. 121

---
+++ Chapter_4/allMyCats1.py
@@ -0,0 +1,19 @@
+#! /usr/bin/python3
+
+print('Введите имя кошки 1:')
+catName1 = input()
+print('Введите имя кошки 2:')
+catName2 = input()
+print('Введите имя кошки 3:')
+catName3 = input()
+print('Введите имя кошки 4:')
+catName4 = input()
+print('Введите имя кошки 5:')
+catName5 = input()
+print('Введите имя кошки 6:')
+catName6 = input()
+print('Имена кошек такие: ')
+print(catName1 + ' ' + catName2 + ' ' + catName3 +
+      ' ' + catName4 + ' ' + catName5 + ' ' + catName6)
+
+input()
