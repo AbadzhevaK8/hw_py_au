26e23dc34172b6064bb1c14bfcac6f26cb519694
Kate <make.me.loco@gmail.com>
2019-03-02 18:54:58 +0300

    Ch.8, p. 243

--- Chapter_8/randomQuizGenerator.py
+++ Chapter_8/randomQuizGenerator.py
@@ -1,2 +1,28 @@
 #! python3
 #! /usr/bin/python3
+
+# randomQuizGenerator.py - Создает экзаменационные билеты с вопросами и ответами, расположенными в случайном порядке, вместе с ключами ответов.
+
+import random
+
+# Данные билета. Ключи - названия штатов, а знечения - столицы.
+capitals = {'Alabama': 'Montgomery', 'Alaska': 'Juneau', 'Arizona': 'Phoenix', 'Arkansas': 'Little Rock', 'California': 'Sacramento', 'Colorado': 'Denver', 'Connecticut': 'Hartford', 'Delaware': 'Dover', 'Florida': 'Tallahassee', 'Georgia': 'Atlanta', 'Hawaii': 'Honolulu', 'Idaho': 'Boise', 'Illinois': 'Springfield', 'Indiana': 'Indianapolis', 'Iowa': 'Des Moines', 'Kansas': 'Topeka', 'Kentucky': 'Frankfort', 'Louisiana': 'Baton Rouge', 'Maine': 'Augusta', 'Maryland': 'Annapolis', 'Massachusetts': 'Boston', 'Michigan': 'Lansing', 'Minnesota': 'Saint Paul', 'Mississippi': 'Jackson', 'Missouri': 'Jefferson City',
+            'Montana': 'Helena', 'Nebraska': 'Lincoln', 'Nevada': 'Carson City', 'New Hampshire': 'Concord', 'New Jersey': 'Trenton', 'New Mexico': 'Santa Fe', 'New York': 'Albany', 'North Carolina': 'Raleigh', 'North Dakota': 'Bismarck', 'Ohio': 'Columbus', 'Oklahoma': 'Oklahoma City', 'Oregon': 'Salem', 'Pennsylvania': 'Harrisburg', 'Rhode Island': 'Providence', 'South Carolina': 'Columbia', 'South Dakota': 'Pierre', 'Tennessee': 'Nashville', 'Texas': 'Austin', 'Utah': 'Salt Lake City', 'Vermont': 'Montpelier', 'Virginia': 'Richmond', 'Washington': 'Olympia', 'West Virginia': 'Charleston', 'Wisconsin': 'Madison', 'Wyoming': 'Cheyenne'}
+
+# Генерация 35 файлов билетов.
+for quizNum in range(35):
+    # Создать файлы билетов и ключей ответов.
+    quizFile = open('capitalsquiz%s.txt' % (quizNum + 1), 'w')
+    answerKeyFile = open('capitalsquiz_answers%s.txt' % (quizNum + 1), 'w')
+
+    # Записать заголовок билета.
+    quizFile.write('Имя:\n\nДата:\n\nКурс:\n\n')
+    quizFile.write(
+        (' ' * 15) + 'Проверка знания столиц штатов (Билет %s)' % (quizNum + 1))
+    quizFile.write('\n\n')
+
+    # Перемешать порядок следования штатов.
+    states = list(capitals.keys())
+    random.shuffle(states)
+
+    # TODO: Организовать цикл по всем 50 штатам, создавая вопрос для каждого из них.
