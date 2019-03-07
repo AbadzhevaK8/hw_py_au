9381d877611e575f89b36faa81c79a2afd64ea60
Artem <impersonalis@yandex.ru>
2019-03-07 21:52:39 +0300

    Packages.

--- Chapter_9/rename_project/renameDates.py
+++ Chapter_9/rename_project/renameDates.py
@@ -1,13 +1,1 @@
 #! python3
-
-import os
-
-for folderName, subfolders, filenames in os.walk('C:\\Users\\AbadzhevaE\\delicious'):
-    print('Текущая папка — ' + folderName)
-
-    for subfolder in subfolders:
-        print('Подпапка ' + folderName + ': ' + subfolder)
-    for filename in filenames:
-        print('Файл внутри ' + folderName + ': '+ filename)
-
-    print('')
