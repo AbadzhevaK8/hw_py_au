686fddb148f3236fc16c5a4fa348556cc75053fb
Kate <make.me.loco@gmail.com>
2019-03-12 12:53:15 +0300

    Ch.9, p.272

--- Chapter_9/backupToZip_project/backupToZip.py
+++ Chapter_9/backupToZip_project/backupToZip.py
@@ -3,14 +3,16 @@
 # -*- coding: utf-8 -*-
 # backupToZip - копирует папку вместе со всем ее содержимым в zip-файл с инкременируемым номером копии файла.
 
-import zipfile, os
+import zipfile
+import os
+
 
 def backupToZip(folder):
+    
     # создание резервной копии всего содержимого папки "folder" в виде архива.
-    folder = os.path.abspath(folder) # абсолютный путь к папке
+    folder = os.path.abspath(folder)  # абсолютный путь к папке
 
     # определяем,как надо назвать архив
-
     number = 1
     while True:
         zipFileName = os.path.basename(folder) + '_' + str(number) + '.zip'
@@ -22,7 +24,19 @@ def backupToZip(folder):
     print('Создается файл %s...' % (zipFileName))
     backupZip = zipfile.ZipFile(zipFileName, 'w')
 
-    # TODO: добавить папку со всем содержимым в архив
+    # добавить папку со всем содержимым в архив
+    for foldername, subfolders, filenames in os.walk(folder):
+        print('Добавление файлов из папки %s...' % (foldername))
+        # добавить в архив текущую папку.
+        backupZip.write(foldername)
+        # в архив все файлы из данной папки.
+        for filename in filenames:
+            newBase = os.path.basename(folder) + '_'
+            if filename.startswith(newBase) and filename.endswith('.zip'):
+                continue  # не создавать резервные копии самих архивов.
+            backupZip.write(os.path.join(foldername, filename))
+    backupZip.close()
     print('Готово.')
 
-backupToZip ('C:\\путь_к_папке')
+
+backupToZip('C:\\Users\\AbadzhevaE\\Downloads\\hw_py_au\\Chapter_9')
