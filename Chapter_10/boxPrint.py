c3c7c763f1f2888daf47cfe42dd7d7a1cc0ab483
Kate <make.me.loco@gmail.com>
2019-03-12 18:41:04 +0300

    Ch.10, p.276

---
+++ Chapter_10/boxPrint.py
@@ -0,0 +1,11 @@
+#! python3
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+
+def boxPrint(symbol, width, height):
+    if len(symbol) != 1:
+        raise Exception('Переменная symbol должна быть односимвольной строкой.')
+    if width <= 2:
+        raise Exception('Значение width должно превышать 2.')
+    if condition:
+        pass
