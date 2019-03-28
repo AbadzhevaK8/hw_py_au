20b7434b46636b52708be34256572727e44773ab
Unknown <make.me.loco@gmail.com>
2019-03-28 22:48:51 +0300

    Ch.12, p.357

--- Chapter_12/project_2/updateProduce.py
+++ Chapter_12/project_2/updateProduce.py
@@ -4,11 +4,18 @@
 import openpyxl
 
 wb = openpyxl.load_workbook('produceSales.xlsx')
-sheet = wb.get_sheet_by_name('Лист')
+sheet = wb.get_sheet_by_name('Sheet')
 
 # Типы продукции и их обновленные цены.
-PRICE_UPDATES = {'Лимон': 3.07,
-                'Сельдерей': 1.19,
-                'Чеснок': 1.27}
+PRICE_UPDATES = {'Lemon': 3.07,
+                 'Celery': 1.19,
+                 'Garlic': 1.27}
 
-# TODO: создать цикл по строкам и обновить цены.
+# создать цикл по строкам и обновить цены.
+for rowNum in range(2, sheet.max_row): # пропустить первую строку
+    produceName = sheet.cell(row=rowNum, column=1).value
+    if produceName in PRICE_UPDATES:
+        sheet.cell(row=rowNum, column=2).value = PRICE_UPDATES[produceName]
+
+wb.save('updatedProduceSales.xlsx')
+ 
