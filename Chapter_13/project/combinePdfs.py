cc6366d0537c6f6b71c0479dec9fa66cad2c9ea8
Unknown <make.me.loco@gmail.com>
2019-04-03 23:22:31 +0300

    Ch.13, p.386

--- Chapter_13/project/combinePdfs.py
+++ Chapter_13/project/combinePdfs.py
@@ -1,4 +1,4 @@
-#!python3
+  #!python3
 # combinePdfs.py - обьединяет все пдфки из одного каталога в один файл без титульных листов.
 
 import PyPDF2
@@ -23,4 +23,7 @@ for filename in pdfFiles:
         pageObj = pdfReader.getPage(pageNum)
         pdfWriter.addPage(pageObj)
 
-# TODO: сохранить результирующий документ в файл
+# сохранить результирующий документ в файл
+pdfOutput = open('allminutes.pdf', 'wb')
+pdfWriter.write(pdfOutput)
+pdfOutput.close()
