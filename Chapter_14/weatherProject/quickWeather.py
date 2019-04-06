63951377962d939aa03c5c0b03ce02f87c484c27
Unknown <make.me.loco@gmail.com>
2019-04-06 23:23:01 +0300

    Ch.14, p.422

--- Chapter_14/weatherProject/quickWeather.py
+++ Chapter_14/weatherProject/quickWeather.py
@@ -1,8 +1,11 @@
 #! python3
 # quickWeather.py - выводит прогноз погоды для заданного населенного пункта.
 
-import json, requests, sys
+import json
+import requests
+import sys
 
+API_KEY = '6c11597eb84c82ced354a5db5e302ef7'
 # определение названия населенного пункта из аргументов командной строки.
 if len(sys.argv) < 2:
     print('Использование: quickWeather.py местоположение')
@@ -10,7 +13,8 @@ if len(sys.argv) < 2:
 location = ' '.join(sys.argv[1:])
 
 # загружаем json-данные из api сайта openweathermap.org.
-url ='http://api.openweathermap.org/data/2.5/forecast/daily?q=%s&cnt=3&APPID=a4c0639c9f32776a6b3816e0ffa34d26' % (location)
+url = 'http://api.openweathermap.org/data/2.5/forecast/daily?q={cityName}&cnt=3&APPID={key}'.format(
+    cityName=location, key=API_KEY)
 response = requests.get(url)
 response.raise_for_status()
 
