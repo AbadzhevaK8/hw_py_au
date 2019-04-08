5935c541dc5690625a0b6fc69e384eccc0d10915
Unknown <make.me.loco@gmail.com>
2019-04-08 22:57:46 +0300

    Ch.15, p.429

--- Chapter_15/stopWatch.py
+++ Chapter_15/stopWatch.py
@@ -10,3 +10,15 @@ print('Отсчет начат.')
 startTime = time.time() # получение времени начала первого замера
 lastTime = startTime
 lapNum = 1
+
+# начало отслеживания замеров
+try:
+    while True:
+        input()
+        lapTime = round(time.time() - lastTime, 2)
+        totalTime = round(time.time() - startTime, 2)
+        print('Замер #%s: %s (%s)' % (lapNum, totalTime, lapTime), end='')
+        lapNum += 1
+        lastTime = time.time() # переустанавливаем время последнего замера
+except KeyboardInterrupt:
+    
