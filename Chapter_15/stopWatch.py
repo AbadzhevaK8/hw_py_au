2880889d7e8f8ccf36b9e0b5467d4c3d1f1acb5b
Unknown <make.me.loco@gmail.com>
2019-04-10 21:59:27 +0300

    Ch.15, p.434

--- Chapter_15/stopWatch.py
+++ Chapter_15/stopWatch.py
@@ -5,9 +5,9 @@ import time
 
 # отображение инструкции по использованию программы.
 print('Чтобы начать отсчет, нажмите клавишу ENTER. Впоследствии для имитации щелчков кнопки секундомера нажимайте клавишу ENTER. Для выхода из программы нажмите клавиши Ctrl+C.')
-input() # нажатие клавиши enter начинает отсчет
+input()  # нажатие клавиши enter начинает отсчет
 print('Отсчет начат.')
-startTime = time.time() # получение времени начала первого замера
+startTime = time.time()  # получение времени начала первого замера
 lastTime = startTime
 lapNum = 1
 
@@ -19,7 +19,7 @@ try:
         totalTime = round(time.time() - startTime, 2)
         print('Замер #%s: %s (%s)' % (lapNum, totalTime, lapTime), end='')
         lapNum += 1
-        lastTime = time.time() # переустанавливаем время последнего замера
+        lastTime = time.time()  # переустанавливаем время последнего замера
 except KeyboardInterrupt:
     # обработать исключение ctrl+c, чтобы предотвратить отображение его сообщений
     print('\nГотово.')
