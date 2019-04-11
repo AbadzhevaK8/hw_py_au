22f8e7c1d6d1c2620a2055b3d9013b6113a80312
Kate <make.me.loco@gmail.com>
2019-04-11 19:01:19 +0300

    Ch.15, p.442

--- Chapter_15/multidownloadXkcd.py
+++ Chapter_15/multidownloadXkcd.py
@@ -1,1 +1,9 @@
 #! python3
+# multidownloadXkcd.py - загружает комиксы в несколько потоков.
+
+import requests, os, bs4, threading
+
+os.makedirs('xkcd', exist_ok=True) # сохраняем комиксы в папке xkcd
+
+def downloadXkcd(arg):
+    pass
